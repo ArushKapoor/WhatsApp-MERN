1c4aaf3231209ac110bdc06080579220e44ad281
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2020-09-07 03:05:41 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDj8g8Jimpv1Wbz5cMgQ-D08TzrWp6UGz8",
+  authDomain: "whatsapp-mern-b90f8.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-b90f8.firebaseio.com",
+  projectId: "whatsapp-mern-b90f8",
+  storageBucket: "whatsapp-mern-b90f8.appspot.com",
+  messagingSenderId: "922351401212",
+  appId: "1:922351401212:web:22028ac8a5714e9dab7f79",
+};
